a1fe38f04989985d6c1330734bb1a961b119f0c6
Aracely Cruz <yuki_oh91@hotmail.com>
2024-05-23 12:00:57 -0600

    update cardSection

--- frontend/src/app/components/cardsSection.tsx
+++ frontend/src/app/components/cardsSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Card, CardHeader, CardBody, Avatar } from "@nextui-org/react";
 import React from "react";
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import CardsSection from "@/components/cardsSection";
+import CardsSection from "@/app/components/cardsSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-50 ">
-      <section className="flex flex-col items-center justify-center gap-7 ">
-        <span className="text-custom-blue font-raleway text-lg font-semibold leading-6 text-left">
-          Unidos por la pasión, el esfuerzo y la amistad
-        </span>
-        <h1 className="font-inter inter text-7xl font-bold leading-snug text-center">
-          ¡Un lugar para todos!
+      <section className="flex flex-col items-center justify-center gap-y-32 ">
+        <h1 className="font-inter inter text-7xl font-bold leading-snug text-center m-bottom-7">
+          <span className="text-custom-blue font-raleway text-lg font-semibold leading-6 text-left inline-block">
+            Unidos por la pasión, el esfuerzo y la amistad
+          </span>
+          <span className="block">¡Un lugar para todos!</span>
         </h1>
+
         {<CardsSection />}
       </section>
     </main>
