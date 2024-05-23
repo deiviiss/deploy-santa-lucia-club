a9f65205f66fe51d69dfd921fde657f834c72857
Adel Fetner <adelfetner@gmail.com>
2024-05-23 14:55:28 -0300

    chore: Add NextUIProvider and NavBar component to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/navBar/NavBar";
+import { NextUIProvider } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} light`}>
+        <NextUIProvider>
+          <NavBar />
+          {children}
+        </NextUIProvider>
+      </body>
     </html>
   );
 }
