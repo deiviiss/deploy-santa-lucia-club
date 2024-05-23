4cd32f5716c42e963a8f822cb49932fce4d40670
Mauro Laime <maurol.dev@gmail.com>
2024-05-23 16:38:57 -0300

    Create footer component

---
+++ frontend/src/components/footer/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as YoutubeIcon } from "./youtubeIcon";
+export { default as FacebookIcon } from "./facebookIcon";
+export { default as LinkedinIcon } from "./linkedinIcon";
+export { default as InstagramIcon } from "./instagramIcon";
+export { default as GithubIcon } from "./githubIcon";
