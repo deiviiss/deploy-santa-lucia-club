9942b672a8f3eaff9cd64dca391143ce77d5eee8
Aracely Cruz <yuki_oh91@hotmail.com>
2024-06-02 23:20:07 -0600

    add carousel and final styles to cards section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import CardsSection from "@/components/cardsSection/cardsSection";
 
 export default function Home() {
   return (
-    <main className="mobile:p-24 flex min-h-screen flex-col items-center justify-between p-0 bg-slate-50 ">
+    <main className="mobile:p-24 flex min-h-screen flex-col items-center justify-between p-0 bg-gradient-custom ">
       <CardsSection />
     </main>
   );
