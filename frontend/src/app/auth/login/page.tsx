75117760c20bbd4a83e2fa0036a58822a128d890
David Hilera <david.hilera@hotmail.com>
2024-06-04 18:22:49 -0600

    chore: Update login and user components
    This commit updates the login and user components to use 'username' instead of 'email' and integrates the login function with the backend API. It also refactors import statements for consistency and adds .env files to .gitignore.
    
    Changes:
    - Updated login component to use 'username' instead of 'email'
    - Integrated login function with backend API
    - Refactored import statements in several components
    - Added .env to .gitignore

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,15 +1,23 @@
 import { LoginForm } from "@/components";
+import Image from "next/image";
 
 export default function RegisterUserPage() {
   return (
-    <main className="flex w-full items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 py-[71px] max-h-[1150px] overflow-hidden">
+    <main className="flex w-full items-center justify-center bg-gradient-to-b from-[#FFFFFF] via-[#CDEDF5] to-[#C1E9F2] ">
+      <div className="grid grid-cols-1 md:grid-cols-2 py-10 overflow-hidden gap-6 xl:gap-12 w-full">
         <LoginForm />
 
         {/*  circle */}
-        <div className="hidden md:block md:w-[1150px] md:h-[1150px] relative overflow-hidden">
-          <div className="w-[1220px] h-[1220px] bg-[#87868A] rounded-full absolute -top-60 left-20">
+        <div className="hidden md:block md:w-[793px] md:h-[724px] relative overflow-hidden">
+          <div className="w-[920px] h-[920px] bg-[#87868A] rounded-full absolute left-28 overflow-hidden">
+            <div className="relative w-[720px] h-[720px]">
+              <div className="w-[720px] h-[720px] absolute md:-left-60 lg:-left-40  xl:-left-20 2xl:-left-5">
+                <Image src="/img-form-login.jpg" alt="form-login" layout="fill" className="h-full" />
+              </div>
+
+            </div>
           </div>
+
         </div>
       </div>
 
