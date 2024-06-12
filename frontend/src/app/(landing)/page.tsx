1412c554b4e2f55cb08cea46f4e0ea4e33d19656
Aracely Cruz <yuki_oh91@hotmail.com>
2024-06-11 22:20:20 -0600

    benefits and promotions final styles

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import { MembershipTypes } from "@/components";
 import Banner from "@/components/Banner/Banner";
+import { Spinner } from "@nextui-org/spinner";
 import CardsSection from "@/components/cardsSection/cardsSection";
 import { Faq } from "@/components/Faq/Faq";
 import { BenefitsSection, HeroSection } from "@/components";
@@ -14,14 +15,17 @@ const userData = {
 };
 
 export default async function Home() {
-
   const UserStatus = await getUserStatus(
     "793d7d6d-96b8-413a-bd6e-8f9de5ce5264"
   );
-  const { membershipTypes } = await getMemberShipTypes()
+  const { membershipTypes } = await getMemberShipTypes();
 
   if (membershipTypes === null) {
-    return <div>Loading...</div>;
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <Spinner size="lg" label="Loading..." color="warning" />
+      </div>
+    );
   }
 
   return (
